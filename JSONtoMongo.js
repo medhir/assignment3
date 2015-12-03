3f36b0b709ae1406fbdf977adabe2195a4f80791
Medhir Bhargava <medhirbhargava@gmail.com>
2015-12-02 19:24:33 -0500

    complete schema and tests

---
+++ JSONtoMongo.js
@@ -0,0 +1,40 @@
+'use strict';
+/* 
+  Import modules/files you may need to correctly run the script. 
+  Make sure to save your DB's uri in the config file, then import it with a require statement!
+ */
+var fs = require('fs'),
+    mongoose = require('mongoose'), 
+    Schema = mongoose.Schema, 
+    Listing = require('./ListingSchema.js'), 
+    uri = require('./config.js').db.uri;
+
+/* Connect to your database */
+mongoose.connect(uri);
+
+/* 
+  Instantiate a mongoose model for each listing object in the JSON file, 
+  and then save it to your Mongo database 
+ */
+
+fs.readFile('listings.json', 'utf8', function(err, data) {
+  if (err) throw err;
+  var parsed = JSON.parse(data);
+  var listingData = parsed.entries;
+
+  listingData.forEach(function(listing, index) {
+    new Listing({
+      name: listing.name, 
+      code: listing.code, 
+      address: listing.address
+    }).save(function(err) {
+      if (err) throw err;
+      console.log('Document saved!');
+    });
+  });
+});
+
+/* 
+  Once you've written + run the script, check out your MongoLab database to ensure that 
+  it saved everything correctly. 
+ */
---
+++ listing.model.test.js
@@ -0,0 +1,66 @@
+var should = require('should'), 
+    mongoose = require('mongoose'), 
+    Listing = require('./ListingSchema'), 
+    config = require('./config');
+
+var listing = {
+  code: 'LBW',
+  name: 'Library West', 
+  address: '1545 W University Ave, Gainesville, FL 32603, United States'
+};
+
+describe('Listing Schema', function() {
+
+  before(function(done) {
+    mongoose.connect(config.db.uri);
+    done();
+  });
+
+  describe('Saving to database', function() {
+    /*
+      Mocha's default timeout for tests is 2000ms. To ensure that the tests do not fail 
+      prematurely, we can increase the timeout setting with the method this.timeout()
+     */
+    this.timeout(10000);
+
+    it('saves properly when code and name provided', function(done){
+      new Listing({
+        name: listing.name, 
+        code: listing.code
+      }).save(function(err){
+        should.not.exist(err);
+        done();
+      });
+    });
+
+    it('saves properly when all three properties provided', function(done){
+      new Listing(listing).save(function(err){
+        should.not.exist(err);
+        done();
+      });
+    });
+
+    it('throws an error when name not provided', function(done){
+      new Listing({
+        code: listing.code
+      }).save(function(err){
+        should.exist(err);
+        done();
+      })
+    });
+
+    it('throws an error when code not provided', function(done){
+      new Listing({
+        name: listing.name
+      }).save(function(err){
+        should.exist(err);
+        done();
+      })
+    });
+
+  });
+
+  afterEach(function(done) {
+    Listing.remove().exec(done);
+  });
+});
